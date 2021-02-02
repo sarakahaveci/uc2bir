ec2ca67eb8ace86b80f32da23f741be30660ca7a
arge01 <gevenci.arif@gmail.com>
2021-02-02 22:40:12 +0300

    top promotion

--- src/app/sub-page/home/TopPromotion.jsx
+++ src/app/sub-page/home/TopPromotion.jsx
@@ -19,24 +19,18 @@ const TopPromotion = (props) => {
         <div className="row">
           <div className="col-lg-6">
             <Title variant="h3" component="h3" lineDisable textLeft>
-              ..VE YENİ BİR SEN
+              Başarmak için başla!
             </Title>
             <Title fontWeight="normal" variant="h4" component="h4" textLeft>
-              Salon VE/Veya Eğitmen Video Alanı
+              {/* "" */}
             </Title>
             <Text color="blue" fontWeight="500" fontSize="20px">
-              Your body hears everything that your mind says. You have to have a
-              positive attitude if you want to achieve your goals.
+              {/* "" */}
             </Text>
             <Text fontWeight="500" fontSize="20px" color="gray1">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo cons ut labore et dolore magna
-              aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-              laboris nisi ut aliqui….
+              {/* "" */}
               <br />
-              <Button className="p-0" variant="link" text="İNCELE" />
+              {/*<Button className="p-0" variant="link" text="İNCELE" />*/}
             </Text>
           </div>
           <div className=".slider-p0 col-lg-6 d-flex">
