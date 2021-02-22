433d5a349e7480ea27624a1c227d4fc1672e474a
Furkan <furkan_onbasilar@hotmail.com>
2021-02-22 11:39:16 +0300

    Add ThemeProvider

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Layout from './views/Layout';
 import { useDispatch } from 'react-redux';
 import { setUserDetailsFromStorage } from 'actions';
+import { ThemeProvider } from 'styled-components';
 
 import { ScrollToTop } from 'components';
 //views
@@ -14,6 +15,7 @@ import ProfRegister from './views/ProfRegister';
 import NotFoundPage from './views/NotFoundPage';
 import ForgotPassword from 'views/ForgotPassword';
 import Profile from 'views/Profile';
+import { theme } from 'utils';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -25,18 +27,24 @@ const App = () => {
   return (
     <ScrollToTop>
       <Router>
-        <Layout>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/info" component={Info} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/profesyonel/register" component={ProfRegister} />
-            <Route exact path="/forgot-password" component={ForgotPassword} />
-            <Route exact path="/profile/:id" component={Profile} />
-            <Route component={NotFoundPage} />
-          </Switch>
-        </Layout>
+        <ThemeProvider theme={theme}>
+          <Layout>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/info" component={Info} />
+              <Route exact path="/register" component={Register} />
+              <Route
+                exact
+                path="/profesyonel/register"
+                component={ProfRegister}
+              />
+              <Route exact path="/forgot-password" component={ForgotPassword} />
+              <Route exact path="/profile/:id" component={Profile} />
+              <Route component={NotFoundPage} />
+            </Switch>
+          </Layout>
+        </ThemeProvider>
       </Router>
     </ScrollToTop>
   );
