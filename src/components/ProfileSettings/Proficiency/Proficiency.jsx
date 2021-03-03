4108985df285a40374f69b592c2e9d615edde09e
Furkan <furkan_onbasilar@hotmail.com>
2021-03-03 21:24:44 +0300

    Add Proficiency

--- src/components/ProfileSettings/Proficiency/Proficiency.jsx
+++ src/components/ProfileSettings/Proficiency/Proficiency.jsx
@@ -1,19 +1,83 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
 
-import { Text, Accordion } from 'components';
+import { Text, Accordion, Title } from 'components';
+import ProficiencyImage from 'assets/proficiency.png';
+import ProficiencyRow from './ProficiencyRow';
+
+const mockData = [
+  {
+    sport: 'Fitness',
+    sportIcon: 1,
+    infoArr: [
+      {
+        name: 'Medikal Egzersiz Uzmanlığı',
+        status: 'accepted',
+      },
+      {
+        name: 'Medikal Egzersiz Uzmanlığı',
+        status: 'pending',
+      },
+
+      {
+        name: 'Medikal Egzersiz Uzmanlığı',
+        status: 'pending',
+      },
+    ],
+  },
+  {
+    sport: 'Yoga',
+    sportIcon: 1,
+    infoArr: [
+      {
+        name: 'Medikal Egzersiz Uzmanlığı',
+        status: 'accepted',
+      },
+      {
+        name: 'Medikal Egzersiz Uzmanlığı',
+        status: 'pending',
+      },
+
+      {
+        name: 'Medikal Egzersiz Uzmanlığı',
+        status: 'pending',
+      },
+    ],
+  },
+];
 
 const Proficiency = () => {
   return (
-    <div>
-      <Text color="dark" fontSize="1rem"></Text>
+    <div className="proficiency">
+      <Title variant="h5" textAlign="left" className="proficiency__title">
+        Uzmanlıklarım
+      </Title>
 
-      <Text color="dark" fontSize="0.9rem" fontWeight="500">
-        Branşlarınıza ait uzmanlıklarınızı giriniz.
-      </Text>
+      <Row>
+        <Col lg={3}>
+          <img
+            src={ProficiencyImage}
+            width="100%"
+            className="proficiency__img"
+          />
+        </Col>
 
-      <Accordion>
-        <ProficiencyRow />
-      </Accordion>
+        <Col lg={6} className="proficiency__info">
+          <Text color="dark" fontSize="1rem">
+            Uzmanlıklarım
+          </Text>
+
+          <Text color="dark" fontSize="0.9rem" fontWeight="500" mb="10px">
+            Branşlarınıza ait uzmanlıklarınızı giriniz.
+          </Text>
+
+          <Accordion>
+            {mockData.map((item) => (
+              <ProficiencyRow {...item} />
+            ))}
+          </Accordion>
+        </Col>
+      </Row>
     </div>
   );
 };
