42c84c7b311717d9f5b4a57a889852dd4c8a8f82
halilatilla <atillahalil0@gmail.com>
2021-03-22 16:49:38 +0300

    Update BlogDetail page style

--- src/views/BlogDetail.jsx
+++ src/views/BlogDetail.jsx
@@ -1,17 +1,11 @@
 import React, { useEffect } from 'react';
-import {
-  Main,
-  Title,
-  Text,
-  Spinner,
-  BlogCartList,
-  AwesomeIcon,
-} from 'components';
+import { Main, Title, Spinner, BlogCartList, AwesomeIcon } from 'components';
 import styled from 'styled-components/macro';
 import { Col, Container, Row } from 'react-bootstrap';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { getBlogDetail } from 'actions';
+import {decode} from 'html-entities';
 
 import {
   FacebookShareButton,
@@ -21,76 +15,79 @@ import {
 } from 'react-share';
 
 const BlogDetail = ({ match }) => {
-  const detail = useSelector((state) => state?.myBlogs?.detail);
-  const blogs = useSelector((state) => state?.myBlogs?.blogs);
   const dispatch = useDispatch();
-
+  const blogs = useSelector((state) => state?.myBlogs?.blogs);
+  const detail = useSelector((state) => state?.myBlogs?.detail);
+  
   useEffect(() => {
     dispatch(getBlogDetail(match?.params?.seo));
-  }, []);
+  }, [match?.params]);
 
   return (
-    <Main className="blog-detail">
+    <Main>
       <Container>
-        <>
-          {!detail.isLoading ? (
-            <>
-              <Row style={{ marginTop: 30 }}>
-                <Col xs="auto">
-                  <ShareButtons>
-                    <Title fontSize="9pt">Paylaş</Title>
-                    <FacebookShareButton
-                      url={`${window?.location?.origin}/${match?.url}`}
-                      media={detail?.data?.blog?.photo}
-                      windowWidth={1000}
-                      windowHeight={730}
-                    >
-                      <AwesomeIcon.Facebook />
-                    </FacebookShareButton>
+        {!detail.isLoading ? (
+          <div className="blog-detail__wrapper">
+            <div className="blog-detail__img">
+              <img src={detail?.data?.blog?.photo} />
+            </div>
+            <Row>
+              <Col xs="auto">
+                <ShareButtons>
+                  <Title fontSize="9pt">Paylaş</Title>
+                  <FacebookShareButton
+                    url={`${window?.location?.origin}/${match?.url}`}
+                    media={detail?.data?.blog?.photo}
+                    windowWidth={1000}
+                    windowHeight={730}
+                  >
+                    <AwesomeIcon.Facebook />
+                  </FacebookShareButton>
 
-                    <TwitterShareButton
-                      url={`${window?.location?.origin}/${match?.url}`}
-                      media={detail?.data?.blog?.photo}
-                      windowWidth={1000}
-                      windowHeight={730}
-                    >
-                      <AwesomeIcon.Twitter />
-                    </TwitterShareButton>
+                  <TwitterShareButton
+                    url={`${window?.location?.origin}/${match?.url}`}
+                    media={detail?.data?.blog?.photo}
+                    windowWidth={1000}
+                    windowHeight={730}
+                  >
+                    <AwesomeIcon.Twitter />
+                  </TwitterShareButton>
 
-                    <InstapaperShareButton
-                      url={`${window?.location?.origin}/${match?.url}`}
-                      media={detail?.data?.blog?.photo}
-                      windowWidth={1000}
-                      windowHeight={730}
-                    >
-                      <AwesomeIcon.Instagram />
-                    </InstapaperShareButton>
+                  <InstapaperShareButton
+                    url={`${window?.location?.origin}/${match?.url}`}
+                    media={detail?.data?.blog?.photo}
+                    windowWidth={1000}
+                    windowHeight={730}
+                  >
+                    <AwesomeIcon.Instagram />
+                  </InstapaperShareButton>
 
-                    <LinkedinShareButton
-                      url={`${window?.location?.origin}/${match?.url}`}
-                      media={detail?.data?.blog?.photo}
-                      windowWidth={1000}
-                      windowHeight={730}
-                    >
-                      <AwesomeIcon.Linkedin />
-                    </LinkedinShareButton>
-                  </ShareButtons>
-                </Col>
-                <Col>
-                  <Row>
-                    <Img src={detail?.data?.blog?.photo}></Img>
-                    <Title variant="h4" component="h4" textAlign="left">
-                      {detail?.data?.blog?.title}
-                    </Title>
-                    <Text>{detail?.data?.blog?.detail}</Text>
-                  </Row>
-                </Col>
-              </Row>
-            </>
-          ) : (
-            <Spinner />
-          )}
-        </>
+                  <LinkedinShareButton
+                    url={`${window?.location?.origin}/${match?.url}`}
+                    media={detail?.data?.blog?.photo}
+                    windowWidth={1000}
+                    windowHeight={730}
+                  >
+                    <AwesomeIcon.Linkedin />
+                  </LinkedinShareButton>
+                </ShareButtons>
+              </Col>
+              <Col className="blog-detail__content">
+                <Title variant="h3" component="h3" lineDisable={false}>
+                  {detail?.data?.blog?.title}
+                </Title>
+                <div
+                  className="blog-detail__text"
+                  dangerouslySetInnerHTML={{
+                    __html:decode(detail?.data?.blog?.detail) ,
+                  }}
+                />
+              </Col>
+            </Row>
+          </div>
+        ) : (
+          <Spinner />
+        )}
       </Container>
       <Section>
         <Container fluid>
@@ -106,7 +103,6 @@ const ShareButtons = styled.div`
   height: auto;
   display: flex;
   flex-direction: column;
-  padding: 0 30px;
 
   button {
     margin-bottom: 5px;
@@ -117,12 +113,7 @@ const Section = styled.section`
   width: 100%;
   height: auto;
   padding: 30px;
+  margin-bottom:270px;
 `;
-
-const Img = styled.img`
-  width: 100%;
-  height: auto;
-`;
-
 
 export default BlogDetail;
