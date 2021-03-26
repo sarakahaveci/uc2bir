6cd1bb7ef3506477356fae462c1dfa594ad64ce1
Mustafa Erk <mustafa.erk@bilyoner.com>
2021-03-26 20:21:25 +0300

    Location card

--- src/components/LocationCard/LocationCard.jsx
+++ src/components/LocationCard/LocationCard.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Svg, Text } from 'components';
+import SimpleGoogleMap from 'components/GoogleMaps/SimpleGoogleMap';
 
-const LocationCard = ({ title, addressDetail }) => {
+const LocationCard = ({ title, addressDetail, location, hasMapLocation }) => {
   return (
-    <div className="locationCardWrapper">
-      <div className="d-flex ml-2 p-3">
+    <div class="row locationCardWrapper">
+      <div class="col-lg-2 col-md-3 col-xs-12">
         <Svg.LocationIcon />
-        <div className="infoTextArea">
-          <Text className="titleText">{title}</Text>
-          <Text className="subText">{addressDetail}</Text>
-        </div>
       </div>
+      <div class="infoTextArea col-lg-4  col-md-9 col-xs-12">
+        <Text className="titleText">{title}</Text>
+        <Text className="subText">{addressDetail}</Text>
+      </div>
+      {hasMapLocation && (
+        <div class="col-12 col-lg-6 p-0">
+          <SimpleGoogleMap location={location} />
+        </div>
+      )}
       <div className="knock">
         <Svg.ArrowUpIcon />
       </div>
@@ -26,9 +32,12 @@ LocationCard.propTypes = {
   title: PropTypes.string.isRequired,
   addressDetail: PropTypes.string.isRequired,
   location: PropTypes.object,
+  hasMapLocation: PropTypes.bool,
 };
 
 LocationCard.defaultProps = {
   title: 'Maçka Parkı',
   addressDetail: 'Cevdet Paşa Caddesi No : 52-54 Bebek - İstanbul',
+  hasMapLocation: true,
+  location: { lat: 41.015137, lng: 28.97953 },
 };
