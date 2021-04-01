231c12d368c6300abb180563764cb75dddeca761
Furkan <furkan@4alabs.io>
2021-04-01 19:51:14 +0300

    Delete buttons from ProfileCard

--- src/components/banner/profile-banner/ProfileCard.jsx
+++ src/components/banner/profile-banner/ProfileCard.jsx
@@ -2,11 +2,11 @@
 import React, { useEffect, useState } from 'react';
 
 import styled from 'styled-components/macro';
-import { Button, Svg, Spinner } from 'components';
-import { NavLink, Link } from 'react-router-dom';
+import { Svg, Spinner } from 'components';
+import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
-import Card, { CardFooter, CardInfo } from './Card';
+import Card, { CardInfo } from './Card';
 import { Col } from 'react-bootstrap';
 import { useFileUpload } from 'use-file-upload';
 import FormData from 'form-data';
@@ -22,8 +22,6 @@ const ProfileCard = ({
   location = null,
   children,
 }) => {
-  const reservationAction = () => {};
-  const comment = () => {};
   const auth = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   const accessToken = auth?.accessToken;
@@ -101,40 +99,12 @@ const ProfileCard = ({
       >
         <Svg.Notification />
       </NotificationLink>
-
-      {auth?.user?.type_id !== 1 && (
-        <CardFooter>
-          <Comment onClick={comment} className="list">
-            <Svg.Comment />
-          </Comment>
-          <Button
-            onClick={reservationAction}
-            text="Rezervasyon Yap"
-            className="blue list"
-            style={{ fontSize: '9pt' }}
-          ></Button>
-        </CardFooter>
-      )}
       <Cols>
         <CardInfo name={name} location={location} info={children} />
       </Cols>
     </Card>
   );
 };
-
-const Comment = styled(Link)`
-  background: #fff;
-  display: flex;
-  min-width: 50px;
-  justify-content: center;
-  align-items: center;
-  border-radius: 10px;
-
-  svg {
-    width: 25px;
-    height: 25px;
-  }
-`;
 
 const Cols = styled(Col)`
   height: auto;
