15f49779b417f99a21ccbfb8eba9954818698d8d
Huseyin inciler <huseyininciler@gmail.com>
2021-04-27 11:09:57 +0300

    -calendar reservation detail edited

---
+++ src/constants/sessionType.js
@@ -0,0 +1,12 @@
+
+export const GYM = 'gym';
+export const ONLINE = 'online';
+export const HOME_PARK = 'home_park';
+
+
+export const SESSION_KEYS = {
+  [GYM]: 'Spor Alanı',
+  [ONLINE]: 'Online',
+  [HOME_PARK]: 'Ev/Park',
+};
+
