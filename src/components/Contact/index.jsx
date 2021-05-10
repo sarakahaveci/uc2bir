6878f061f7c05420e0f96197a925524e93439a67
Huseyin inciler <huseyininciler@gmail.com>
2021-05-10 10:39:04 +0300

    -contact multiline character check added

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -181,8 +181,7 @@ export default function Contact() {
                   name="message"
                   label="Mesajınız"
                   rows={8}
-                  defaultValue={message}
-                  onBlur={(e) => setMessage(e.target.value)}
+                  onChange={(e) => setMessage(e.target.value)}
                   required
                   inputProps={{ minLength: 10, maxLength: 2500 }}
                 />
