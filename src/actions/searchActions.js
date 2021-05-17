276fbef2ee7b2bfc525766196ee2fcc20562f1af
mertct11 <54515536+mertct11@users.noreply.github.com>
2021-05-17 18:27:55 +0300

    search error fix

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -1,6 +1,6 @@
 import { SEARCH_RESULTS, HTTP_REQUEST } from 'constants/actionTypes';
 import { toast } from 'react-toastify';
-export const getSearchResults = (keyword, successCallback, errorCallback) => async (
+export const getSearchResults = (keyword, successCallback) => async (
   dispatch
 ) => {
   const url = `/user/search?keyword=${keyword}`;
@@ -12,16 +12,17 @@ export const getSearchResults = (keyword, successCallback, errorCallback) => asy
       url,
       label: SEARCH_RESULTS,
       transformData: (data) => data.data,
-      callBack: (data) => { 
-        if (!(data?.data?.blog.length == 0 &&
-          data?.data?.dt.length == 0 &&
-          data?.data?.dt_package.length == 0 &&
-          data?.data?.gym.length == 0 &&
-          data?.data?.pt.length == 0 &&
-          data?.data?.pt_package.length == 0)) { successCallback(); }
-        else {
-          errorCallback();
-        }
+      callBack: () => { 
+        // if (!(data?.data?.blog.length == 0 &&
+        //   data?.data?.dt.length == 0 &&
+        //   data?.data?.dt_package.length == 0 &&
+        //   data?.data?.gym.length == 0 &&
+        //   data?.data?.pt.length == 0 &&
+        //   data?.data?.pt_package.length == 0)) { successCallback(); }
+        // else {
+        //   errorCallback();
+        // }
+        successCallback();
       },
       errorHandler: (error) => {
         toast.error(error.message, {
