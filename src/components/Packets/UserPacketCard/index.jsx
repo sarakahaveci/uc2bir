fec8e7b5b334cc5e1a2499c4c0bb2b65eb40bcdb
mmuratmutlu <murat.mutlu@4alabs.io>
2021-06-01 11:56:09 +0300

    eslint fix

--- src/components/Packets/UserPacketCard/index.jsx
+++ src/components/Packets/UserPacketCard/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components/macro';
 import { device } from 'utils';
 import { useHistory } from 'react-router-dom';
