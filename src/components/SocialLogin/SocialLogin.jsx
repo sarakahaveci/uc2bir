5ecf6539d74a57426c81e221a7ed2a8297d2bd35
mmuratmutlu <murat.mutlu@4alabs.io>
2021-06-02 12:24:16 +0300

    dietisyen brancş kaldırıldı

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -11,13 +11,13 @@ import { Button, AwesomeIcon } from 'components';
 import { socialLogin } from 'actions';
 
 firebase.initializeApp({
-    apiKey: "AIzaSyAGmRcb5e41Jcz_LOuS3cXw2Cqcc58zmgs",
-    authDomain: "uc2bir-dd7ee.firebaseapp.com",
-    projectId: "uc2bir-dd7ee",
-    storageBucket: "uc2bir-dd7ee.appspot.com",
-    messagingSenderId: "197190928694",
-    appId: "1:197190928694:web:da3d1769f371f1ecaccb97",
-    measurementId: "G-R79XC04VG0"
+    apiKey: 'AIzaSyAGmRcb5e41Jcz_LOuS3cXw2Cqcc58zmgs',
+    authDomain: 'uc2bir-dd7ee.firebaseapp.com',
+    projectId: 'uc2bir-dd7ee',
+    storageBucket: 'uc2bir-dd7ee.appspot.com',
+    messagingSenderId: '197190928694',
+    appId: '1:197190928694:web:da3d1769f371f1ecaccb97',
+    measurementId: 'G-R79XC04VG0',
 });
 
 const auth = firebase.auth();
