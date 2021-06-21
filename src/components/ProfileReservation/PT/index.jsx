751f4a3af4d33878c532f5d8d8ad14b2fa7cae4a
mmuratmutlu <murat.mutlu@4alabs.io>
2021-06-21 13:08:38 +0300

    yüzmeden online kaldırıldı

--- src/components/ProfileReservation/PT/index.jsx
+++ src/components/ProfileReservation/PT/index.jsx
@@ -550,7 +550,7 @@ const PT = () => {
               <InputContainer disable={reservation?.data?.isSelected}>
                 <Text color="#9B9B9B">{'Branş Seçiniz:'}</Text>
                 <Material.SimpleSelect
-                  items={branchList.branches}
+                  items={reservation?.data?.session == 'online' ? branchList.branches.filter(item => item.id !== 35):branchList.branches}
                   name="branch"
                   defaultValue={reservation?.data?.branch_id}
                   onChange={(e) =>
@@ -563,7 +563,7 @@ const PT = () => {
                 {
                   sessionTypes && sessionTypes.length > 0 &&
                   <Material.SimpleSelect
-                    items={sessionTypes}
+                    items={reservation?.data?.branch_id == 35 ? sessionTypes?.filter(item => item.id !== 'online'):sessionTypes}
                     name="sessionType"
                     defaultValue={reservation?.data?.session}
                     onChange={(e) =>
