f7c6cc1af0d53c3090c9c9b98f3de850c2b89536
mmuratmutlu <murat.mutlu@4alabs.io>
2021-06-23 14:28:31 +0300

    Blog remake

--- src/views/BlogDetail.jsx
+++ src/views/BlogDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Main, Title, Spinner, BlogCartList, AwesomeIcon } from 'components';
+import { Main, Spinner, BlogCartList, AwesomeIcon } from 'components';
 import { Col, Container, Row } from 'react-bootstrap';
 import { decode } from 'html-entities';
 import ReactHtmlParser from 'react-html-parser';
@@ -29,65 +29,77 @@ const BlogDetail = ({ match }) => {
           <div className="blog-top__banner" />
         </section>
         <div className="blog-detail">
+
           <Container>
+
             {!detail.isLoading ? (
               <div className="blog-detail__wrapper">
                 <BlogWrapper>
+                  <div style={{ alignItems: 'center', position: 'absolute', left: '10px', marginLeft: '10px', marginTop: '10px' }}>
+                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} className="blog-detail__share-buttons">
+                      <span style={{ marginBottom: '10px' }}
+                        className="blog-detail__share-title">
+                        Paylaş
+                          </span>
+                      <FacebookShareButton
+                        style={{ marginBottom: '10px' }}
+                        url={`${window?.location?.origin}/${match?.url}`}
+                        media={detail?.data?.blog?.photo}
+                        windowWidth={1000}
+                        windowHeight={730}
+                      >
+                        <AwesomeIcon.Facebook />
+                      </FacebookShareButton>
+
+                      <TwitterShareButton
+                        style={{ marginBottom: '10px' }}
+
+                        url={`${window?.location?.origin}/${match?.url}`}
+                        media={detail?.data?.blog?.photo}
+                        windowWidth={1000}
+                        windowHeight={730}
+                      >
+                        <AwesomeIcon.Twitter />
+                      </TwitterShareButton>
+                      <LinkedinShareButton
+                        style={{ marginBottom: '10px' }}
+
+                        url={`${window?.location?.origin}/${match?.url}`}
+                        media={detail?.data?.blog?.photo}
+                        windowWidth={1000}
+                        windowHeight={730}
+                      >
+                        <AwesomeIcon.Linkedin />
+                      </LinkedinShareButton>
+                    </div>
+                  </div>
+
                   <Row>
                     <Col className="blog-detail__content">
-                      <Title variant="h3" component="h3" lineDisable={false}>
+                      <Title >
                         {detail?.data?.blog?.title}
                       </Title>
-                      <Col xs="auto">
-                        <div className="blog-detail__share-buttons">
-                          <span className="blog-detail__share-title">
-                            Paylaş
-                          </span>
-                          <FacebookShareButton
-                            url={`${window?.location?.origin}/${match?.url}`}
-                            media={detail?.data?.blog?.photo}
-                            windowWidth={1000}
-                            windowHeight={730}
-                          >
-                            <AwesomeIcon.Facebook />
-                          </FacebookShareButton>
+                      <Seperator/>
 
-                          <TwitterShareButton
-                            url={`${window?.location?.origin}/${match?.url}`}
-                            media={detail?.data?.blog?.photo}
-                            windowWidth={1000}
-                            windowHeight={730}
-                          >
-                            <AwesomeIcon.Twitter />
-                          </TwitterShareButton>
-                          <LinkedinShareButton
-                            url={`${window?.location?.origin}/${match?.url}`}
-                            media={detail?.data?.blog?.photo}
-                            windowWidth={1000}
-                            windowHeight={730}
-                          >
-                            <AwesomeIcon.Linkedin />
-                          </LinkedinShareButton>
-                        </div>
-                      </Col>
                     </Col>
                   </Row>{' '}
-                  <text className="blog-detail__text">
+                  <Desc className="blog-detail__text">
                     {ReactHtmlParser(decode(detail?.data?.blog?.detail))}
-                  </text>
+                  </Desc>
                   <div className="blog-detail__img">
                     <img src={detail?.data?.blog?.photo} />
                   </div>
+
                 </BlogWrapper>
               </div>
             ) : (
               <Spinner />
             )}
           </Container>
-          <Container fluid>
-            <BlogCartList blogs={blogs.data.blogs} />
-          </Container>
         </div>
+        <Container fluid>
+          <BlogCartList blogs={blogs.data.blogs} />
+        </Container>
       </Main>
     </div>
   );
@@ -119,3 +131,32 @@ const BlogWrapper = styled.div`
     padding: 20px;
   }
 `;
+const Title = styled.p`
+  width:500px;
+  font-family: 'Poppins', sans-serif;
+  text-align:left;
+  font-size:60px;
+  font-weight:bold;
+  color:white;
+  -webkit-text-stroke-width: 1px;
+  -webkit-text-stroke-color: black;
+  &:first-line{
+    font-size:45px;
+    color:black;
+  }
+`;
+const Seperator = styled.div`
+  width: 150px;
+  border-bottom-style: solid;
+  border-color: var(--blue);
+  border-width: 3px;
+  margin-bottom: 15px;
+  margin-top:15px;
+`;
+const Desc = styled.p`
+  font-size:16px;
+  text-align:left;
+  font-weight:normal;
+  margin-top:30px;
+
+`
