133587f7955c558147e449125f510750d4e02446
mmuratmutlu <murat.mutlu@4alabs.io>
2021-07-27 10:56:55 +0300

    fixesss

--- src/components/ProfileSettings/Reservations/ST/Awaitings.jsx
+++ src/components/ProfileSettings/Reservations/ST/Awaitings.jsx
@@ -26,7 +26,7 @@ const Awaitings = ({ setAwaitingCount }) => {
   const [IsSmallScreen, setIsSmallScreen] = useState(false);
   const [openApprove, setOpenApprove] = useState(undefined);
   const [openReject, setOpenReject] = useState(undefined);
-  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(undefined);
   const [choosenElm, setChoosenElm] = useState(null);
 
   const startOfWeeksArr = () => {
