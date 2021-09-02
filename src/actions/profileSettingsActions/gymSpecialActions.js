149cfc0303369bfc36659c1c8ab1badc7a5c0b4d
Rıfat Can Gürbüz <rifat.gurbuz@4alabs.io>
2021-09-02 15:19:15 +0300

    fix

--- src/actions/profileSettingsActions/gymSpecialActions.js
+++ src/actions/profileSettingsActions/gymSpecialActions.js
@@ -1,32 +1,34 @@
 import { HTTP_REQUEST, UPDATE_GYM_SPECIAL_PRICE } from '../../constants';
 import { toast } from 'react-toastify';
 
-export const updateGymSpecialPrice = ({ price }) => async (dispatch) => {
-  const url = `/user/bs-price`;
+export const updateGymSpecialPrice =
+  ({ price }) =>
+  async (dispatch) => {
+    const url = `/user/bs-price`;
 
-  await dispatch({
-    type: HTTP_REQUEST,
-    payload: {
-      method: 'POST',
-      url,
-      body: { price },
-      label: UPDATE_GYM_SPECIAL_PRICE,
-      callBack: () =>
-        toast.success(
-          'Talebiniz gönderildi incelendikten sonra tarafınıza bildirim gönderilecektir.',
-          {
-            position: 'bottom-right',
-            autoClose: 7000,
-          }
-        ),
-      errorHandler: (res) =>
-        toast.error(
-          res?.message || 'Fiyat talebi gönderilirken hata ile karşılaşıldı',
-          {
-            position: 'bottom-right',
-            autoClose: 4000,
-          }
-        ),
-    },
-  });
-};
+    await dispatch({
+      type: HTTP_REQUEST,
+      payload: {
+        method: 'POST',
+        url,
+        body: { price },
+        label: UPDATE_GYM_SPECIAL_PRICE,
+        callBack: () =>
+          toast.success(
+            'Talebiniz gönderildi; incelendikten sonra tarafınıza bildirim gönderilecektir.',
+            {
+              position: 'bottom-right',
+              autoClose: 7000,
+            }
+          ),
+        errorHandler: (res) =>
+          toast.error(
+            res?.message || 'Fiyat talebi gönderilirken hata ile karşılaşıldı',
+            {
+              position: 'bottom-right',
+              autoClose: 4000,
+            }
+          ),
+      },
+    });
+  };
--- src/components/ActivityCard/index.jsx
+++ src/components/ActivityCard/index.jsx
@@ -37,7 +37,7 @@ export default function ActivityCard({
       <ToolTipLabel>
         {classification} sınıfı {name} branşı için{' '}
         <div style={{ color: '#00b2a9' }}>
-          minimum {minPrice } TL{' '}
+          minimum {minPrice} TL{' '}
           {classification !== 'A' ? `,maksimum ${maxPrice} TL` : null}
         </div>
         aralığında fiyat girebilirisiniz.
