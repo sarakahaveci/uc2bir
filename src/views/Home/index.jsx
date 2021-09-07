f6904def8d48b792f925b61cdad463c0f3036cd2
mmuratmutlu <murat.mutlu@4alabs.io>
2021-09-07 11:57:06 +0300

    test3

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,20 +1,21 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Main from '../../components/Main';
-import Banner from './Banner';
-import TopPromotion from './TopPromotion';
-import Categories from './Categories';
-import PT from './PT';
-import FluidBanner from './FluidBanner';
-import FluidBannerBottom from './FluidBannerBottom';
-import Living from './Living';
-import Packet from './Packet';
-import GroupLesson from './GroupLesson';
-import Dietitians from './Dietitians';
-import VKI from './VKI';
-import Blog from './Blog';
+const Banner = React.lazy(() => import('./Banner'))
+const TopPromotion = React.lazy(() => import('./TopPromotion'))
+const Categories = React.lazy(() => import('./Categories'))
+const PT = React.lazy(() => import('./PT'))
+const FluidBanner = React.lazy(() => import('./FluidBanner'))
+const FluidBannerBottom = React.lazy(() => import('./FluidBannerBottom'))
+const Living = React.lazy(() => import('./Living'))
+const Packet = React.lazy(() => import('./Packet'))
+const GroupLesson = React.lazy(() => import('./GroupLesson'))
+const Dietitians = React.lazy(() => import('./Dietitians'))
+const VKI = React.lazy(() => import('./VKI'))
+const Blog = React.lazy(() => import('./Blog'))
+
 import { getHomeContent, getHomeTags } from 'actions';
 
 import Comments from './Comments';
@@ -23,26 +24,62 @@ const Home = () => {
   const { isAuthenticated } = useSelector((state) => state.auth);
 
   const dispatch = useDispatch();
-
+  function _fallback(){ 
+    return (<div></div>)
+  }
   useEffect(() => {
     dispatch(getHomeContent());
   }, []);
 
   return (
     <Main>
-      <Banner />
+      <Suspense fallback={_fallback()}>
+        <Banner />
+      </Suspense>
+      <Suspense fallback={_fallback()}>
       <Categories />
-      <TopPromotion background />
-      <PT />
-      {isAuthenticated ? '' : <FluidBanner />}
-      <Living />
-      <Packet />
-      <GroupLesson />
-      <Dietitians />
-      <VKI />
-      <Blog />
-      <Comments />
-      {isAuthenticated ? '' : <FluidBannerBottom />}
+      </Suspense>
+      <Suspense fallback={_fallback()}>
+        <TopPromotion background  />
+      </Suspense>
+      <Suspense fallback={_fallback()}>
+        <PT />
+      </Suspense>
+
+      {isAuthenticated ? '' :
+
+        <Suspense fallback={_fallback()}>
+          <FluidBanner />
+        </Suspense>
+      }
+      <Suspense fallback={_fallback()}>
+        <Living />
+      </Suspense>
+      <Suspense fallback={_fallback()}>
+        <Packet />
+      </Suspense>
+      <Suspense fallback={_fallback()}>
+        <GroupLesson />
+      </Suspense>
+      <Suspense fallback={_fallback()}>
+        <Dietitians />
+      </Suspense>
+      <Suspense fallback={_fallback()}>
+        <VKI />
+      </Suspense>
+      <Suspense fallback={_fallback()}>
+        <Blog />
+
+      </Suspense>
+      <Suspense fallback={_fallback()}>
+        <Comments />
+
+      </Suspense>
+      {isAuthenticated ? '' :
+        <Suspense fallback={_fallback()}>
+          <FluidBannerBottom />
+        </Suspense>
+      }
     </Main>
   );
 };
